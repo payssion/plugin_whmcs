f19c164c568f13ff6dc6e72e0947856afc8384e2
Dong <dong.liu@payssion.com>
2015-11-30 07:15:01 +0800

    fixed bugs

--- modules/gateways/payssion/payssion.php
+++ modules/gateways/payssion/payssion.php
@@ -9,7 +9,7 @@ Author URI: https://www.payssion.com
 Release date: 11/11/2015
 */
 
-class Payssion extends Controller {
+class Payssion {
 	public static $_api_key = 'you api key';
 	public static $_secret_key = 'you secret key';
 	
@@ -19,24 +19,31 @@ class Payssion extends Controller {
 	}
 	
 	public static function getConfig($pm_name) {
-		return 	$configarray = array(
+		$configarray = array(
 		"FriendlyName" => array(
 			"Type" => "System",
-			"Value" => $pm_name
+			"Value" => $pm_name . "via Payssion"
+		),
+		"testmode" => array(
+				"FriendlyName" => "Test Mode", 
+				"Type" => "yesno", 
+				"Description" => "Please make sure to use your keys at your sandbox account in order to enable test mode", 
 		));
+		
+		return $configarray;
 	}
 	
 	public static function getLink(&$params, $pm_id) {
 		$address = $params['clientdetails']['address1'].' '.$params['clientdetails']['address2'];
 		$url = '';
-		if (true/*$params['payssion_testmode']*/) {
+		if ($params["payssion_testmode_$pm_id"] == 'on') {
 			$url = 'https://www.payssion.com/payment/create.html';
 		} else {
 			$url = 'http://sandbox.payssion.com/payment/create.html';
 		}
 		
-		$api_key = self::$_api_key;//$params['payssion_api_key'];
-		$secret_key = self::$_secret_key;//$params['payssion_secret_key'];
+		$api_key = self::$_api_key;
+		$secret_key = self::$_secret_key;
 		$source = 'whmcs';
 		
 		// Invoice Parameters
@@ -68,7 +75,7 @@ class Payssion extends Controller {
 		<input type="hidden" name="track_id" value="'.$track_id.'" />
 		<input type="hidden" name="description" value="'.$description.'" />
 		<input type="hidden" name="amount" value="'.$amount.'" />
-		<input type="hidden" name="currency" value="'$currency.'" />
+		<input type="hidden" name="currency" value="'.$currency.'" />
 		<input type="hidden" name="notify_url" value="'.$notify_url.'" />
 		<input type="hidden" name="success_url" value="'.$params['returnurl'].'" />
 		<input type="hidden" name="redirect_url" value="'.$params['returnurl'].'" />
@@ -76,82 +83,5 @@ class Payssion extends Controller {
 		</form>';
 		return $code;
 	}
-}
-
-function payssion_config() {
-	$configarray = array(
-		"FriendlyName" => array(
-			"Type" => "System",
-			"Value" => "PAYSSION"
-		),
-		"payssion_api_key" => array(
-			"FriendlyName" => "API Key",
-			"Type" => "text",
-			"Size" => "32",
-			"Description" => "You can find your API Key in your APP setting"
-		),
-		"payssion_secret_key" => array(
-			"FriendlyName" => "Secret Key",
-			"Type" => "text",
-			"Size" => "32",
-			"Description" => "You can find your Secret Key in your APP setting"
-		)/*,
-	    "payssion_testmode" => array(
-	    		"FriendlyName" => "TestMode", 
-	    		"Type" => "yesno", 
-	    		"Description" => "Please select YES if you are using the sandbox environment")*/
-	);
-	return $configarray;
-}
-function okpay_link($params) {
-	$address = $params['clientdetails']['address1'].' '.$params['clientdetails']['address2'];
-	$url = '';
-	if (true/*$params['payssion_testmode']*/) {
-		$url = 'https://www.payssion.com/payment/create.html';
-	} else {
-		$url = 'http://sandbox.payssion.com/payment/create.html';
-	}
-	
-	$api_key = $params['payssion_api_key'];
-	$secret_key = $params['payssion_secret_key'];
-	$source = 'whmcs';
-	$pm_id = 'payssion';
-	
-	// Invoice Parameters
-	$track_id = $params['invoiceid'];
-	$description = $params["description"];
-	$amount = $params['amount'];
-	$currency = $params['currency'];
-	
-	// Client Parameters
-	$payer_name = $params['clientdetails']['firstname'] . ' ' . $params['clientdetails']['lastname'];
-	$payer_email = $params['clientdetails']['email'];
-	
-	$module_name = $params['paymentmethod'];
-	$system_url = $params['systemurl'];
-	$notify_url = $system_url . '/modules/gateways/callback/' . $module_name . '.php';
-	
-	$arr = array($api_key, $pm_id, $amount, $currency,
-			$track_id, '', $secret_key);
-	$api_sig = implode('|', $arr);
-	
-	$code = 
-	$code = '<form method="post" action="' . $url . '">
-				<input type="hidden" name="source" value="'.$source.'" />
-				<input type="hidden" name="api_key" value="'.$api_key.'" />
-				<input type="hidden" name="api_sig" value="'.$api_sig.'" />
-				<input type="hidden" name="pm_id" value="'.$pm_id.'" />
-				<input type="hidden" name="payer_name" value="'.$payer_name.'" />
-				<input type="hidden" name="payer_email" value="'.$payer_email.'" />
-				<input type="hidden" name="track_id" value="'.$track_id.'" />
-				<input type="hidden" name="description" value="'.$description.'" />
-				<input type="hidden" name="amount" value="'.$amount.'" />
-				<input type="hidden" name="currency" value="'$currency.'" />
-				<input type="hidden" name="notify_url" value="'.$notify_url.'" />
-				<input type="hidden" name="success_url" value="'.$params['returnurl'].'" />
-				<input type="hidden" name="redirect_url" value="'.$params['returnurl'].'" />
-				<input type="submit" value="Pay Now" />
-			</form>';
-	return $code;
 }
 ?>
