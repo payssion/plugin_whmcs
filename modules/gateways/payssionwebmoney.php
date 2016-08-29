306049294fd1a023def1c4a03676f1d1f40f09a0
payssion <github@payssion.com>
2016-08-29 21:53:56 +0800

    added webmoney

---
+++ modules/gateways/payssionwebmoney.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: webmoney for WHMCS
+Description: webmoney payment gateway plugin for WHMCS
+Version: 1.0
+Compatible with: WHMCS 5.3.14, 6.0.2
+Author: Payssion
+Author URI: https://www.payssion.com
+Release date: 11/16/2015
+*/
+
+require_once realpath(dirname(__FILE__)) . "/payssion/payssion.php";
+
+function payssionwebmoney_config() {
+	return Payssion::getConfig('webmoney');
+}
+
+function payssionwebmoney_link($params) {
+	return Payssion::getLink($params, 'webmoney');
+}
+?>
