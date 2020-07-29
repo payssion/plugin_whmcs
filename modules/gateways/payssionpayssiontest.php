d86f5d47be1871c2a6f67f8ff415599f7132db42
payssion <liudong4u@gmail.com>
2020-07-29 16:39:56 +0800

    fix enets_sg issue

---
+++ modules/gateways/payssionpayssiontest.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: alipay for WHMCS
+Description: alipay payment gateway plugin for WHMCS
+Version: 1.0
+Compatible with: WHMCS 5.3.14, 6.0.2
+Author: Payssion
+Author URI: https://www.payssion.com
+Release date: 11/16/2015
+*/
+
+require_once realpath(dirname(__FILE__)) . "/payssion/payssion.php";
+
+function payssionpayssiontest_config() {
+	return Payssion::getConfig('payssion_test');
+}
+
+function payssionpayssiontest_link($params) {
+	return Payssion::getLink($params, 'payssion_test');
+}
+
+function payssionpayssiontest_refund($params) {
+	return Payssion::refund($params);
+}
+
+?>
