1792adc0bf5ab17937b3c4b88ab6eebde7c309ce
payssion <liudong4u@gmail.com>
2020-12-15 17:12:45 +0800

    add bankcard_ru

---
+++ modules/gateways/payssionbankcardru.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: CIS Credit/Bank Card for WHMCS
+Author: Payssion
+Author URI: https://www.payssion.com
+*/
+
+require_once realpath(dirname(__FILE__)) . "/payssion/payssion.php";
+
+function payssionbankcardru_config() {
+	return Payssion::getConfig('Russian Credit/Bank Card');
+}
+
+function payssionbankcardru_link($params) {
+	return Payssion::getLink($params, 'bankcard_ru');
+}
+?>
